fb6a2aa42c846b012adce70224a99b5a0d343db1
ppxr2002 <114091171+ppxr2002@users.noreply.github.com>
2024-04-27 23:01:38 +0300

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,173 @@
+let height = $(window).height();
+let menu = $("*#menu");
+var body = $('body');
+var header = $('header');
+var light = $('#light');
+var dark = $('#dark');
+var image = $('#profil');
+var nom = $('*#nom');
+
+window.onload = ()=>{
+    changeLangue();
+    scrollFenetre("#000");
+    
+    dark.click(()=>{
+        darkMode()
+        scrollFenetre("rgba(255,255,255,0.1)");
+    })
+    light.click(()=>{
+        lightMode()
+        scrollFenetre("rgba(0,0,0,0)");
+    })
+    
+}
+
+function menu_classRemove(){
+    $(menu[0]).removeClass("active");
+    $(menu[1]).removeClass("active");
+    $(menu[2]).removeClass("active");
+    $(menu[3]).removeClass("active");
+    $(menu[4]).removeClass("active");
+    $(menu[5]).removeClass("active");
+    $(menu[6]).removeClass("active");
+    $(menu[7]).removeClass("active");
+}
+
+function changeLangue(){
+    document.getElementById("langue").addEventListener("change", ()=>{
+        let width = $(window).width();
+        let langue = $("#langue option:selected").val();
+        
+        
+        if(langue === "english"){
+            $("*#fr").css("display", "none");
+            $("*#en").css("display", "flex");
+            $("*.mother_tools").css("flex-direction", "column");
+        }
+        if(langue === "french"){
+            $("*#en").css("display", "none");
+            $("*#fr").css("display", "flex");
+            $("*.mother_tools").css("flex-direction", "column");
+        }
+        if(width <= 800){
+            $(".menu").css("display", "none");
+        }
+        else{
+            if(langue === "english"){
+                $(".menu #fr").css("display", "none");
+                $(".menu #en").css("display", "flex");
+            }
+            if(langue === "french"){
+                $(".menu #en").css("display", "none");
+                $(".menu #fr").css("display", "flex");
+            }
+        }
+    });
+}
+
+function scrollFenetre(color){
+    window.addEventListener('scroll', ()=>{
+        let scroll = $(window).scrollTop();
+
+        if(scroll <= 0){
+            $("#header").css("box-shadow", "none");
+            menu_classRemove();
+            $(menu[0]).addClass("active");
+            $(menu[4]).addClass("active");
+        }
+        else{
+            $("#header").css("box-shadow", "0 0px 10px " + color);
+            if(scroll >= (height/2)){
+                menu_classRemove();
+                if(scroll >= ((height*3)/2)){
+                    menu_classRemove();
+                    if(scroll >= (height*2)){
+                        menu_classRemove();
+                        if(scroll >= ((height*5)/2)){
+                            menu_classRemove();
+                            $(menu[3]).addClass("active"); 
+                            $(menu[7]).addClass("active");
+                        }
+                        else{
+                            $(menu[2]).addClass("active");
+                            $(menu[6]).addClass("active");
+                        }
+                    }
+                    else{
+                        $(menu[2]).addClass("active");
+                        $(menu[6]).addClass("active");
+                    }
+                }
+                else{
+                    $(menu[1]).addClass("active");
+                    $(menu[5]).addClass("active");
+                }
+            }
+        }
+    });
+}
+
+function home(){
+    $("html, body").animate({
+        scrollTop: 0
+    }, 800)
+}
+function skills(){
+    $("html, body").animate({
+        scrollTop: height
+    }, 800)
+}
+function projects(){
+    $("html, body").animate({
+        scrollTop: height*2
+    }, 800)
+}
+function contact(){
+    $("html, body").animate({
+        scrollTop: height*3
+    }, 800)
+}
+
+function darkMode(){
+    body.css('background','#000');
+    header.css('background','#000');
+    light.css('display','block');
+    dark.css('display','none');
+    menu.css('color','#fff');
+    nom.css('color','#45ADFF');
+    image.attr('src', './img/imgDark.png');
+    $("#img_menu").attr('src', './img/menuDark.png');
+    $("*.descri").css("color", "#fff")
+    $("select").css("color", "#fff");
+    $("select").css("background", "#000");
+    $("*.tools").css("color", "#45ADFF");
+    $("*#txt").css("color", "#45ADFF");
+    $("*.ligne").css("background", "#45ADFF");
+    $("*#lien").css("background", "#45ADFF");
+}
+function lightMode(){
+    body.css('background','#f8f8f8');
+    header.css('background','#f8f8f8');
+    light.css('display','none');
+    dark.css('display','block');
+    menu.css('color','#000');
+    nom.css('color','#008FFF');
+    image.attr('src','./img/imgLight.png');
+    $("#img_menu").attr('src','./img/menuLight.png');
+    $("*.descri").css("color", "#000")
+    $("select").css("color", "#000");
+    $("select").css("background", "#F8F8F8");
+    $("*.tools").css("color", "#008FFF");
+    $("*#txt").css("color", "#008FFF");
+    $("*.ligne").css("background", "#008FFF");
+    $("*#lien").css("background", "#008FFF");
+}
+
+function menu_open(){
+    $(".flou").css("display", "block");
+    $("#icon_menu").css("display", "none");
+}
+function menu_close(){
+    $(".flou").css("display", "none");
+    $("#icon_menu").css("display", "block");
+}
