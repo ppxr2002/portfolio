d4616c3e4c2e7bdf4f6690e6df1beed9047e31cd
ppxr23 <114091171+ppxr23@users.noreply.github.com>
2024-08-02 15:54:16 +0300

    tairo

--- script/send_mail.js
+++ script/send_mail.js
@@ -1,6 +1,12 @@
 document.getElementById('formContact').addEventListener('submit', function(event) {
     event.preventDefault();
 
+    var recaptchaResponse = grecaptcha.getResponse();
+    if (recaptchaResponse.length === 0) {
+        alert('Please complete the reCAPTCHA.');
+        return;
+    }
+
     emailjs.sendForm('service_8lest3i', 'template_z90rqpt', this)
         .then(function(response) {
             alert('Success! Your message has been sent.');
