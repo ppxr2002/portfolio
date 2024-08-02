a7880187bc04279fbcab9d21f35c30959cb7790c
ppxr23 <114091171+ppxr23@users.noreply.github.com>
2024-08-02 14:10:14 +0300

    ao hoe

--- script/send_mail.js
+++ script/send_mail.js
@@ -1,15 +1,12 @@
-$('#formContact').addEventListener('submit', function(event) {
+document.getElementById('formContact').addEventListener('submit', function(event) {
     event.preventDefault();
 
-        var name = encodeURIComponent($('#name').value);
-        var email = encodeURIComponent($('#email').value);
-        var message = encodeURIComponent($('#message').value);
-
-        var subject = encodeURIComponent($('#motif').value);
-        var to = encodeURIComponent('rotsiniainanajaina@gmail.com');
-        
-        alert(name +" "+ email +" "+ message +" "+ subject +" "+ to)
-        var mailtoLink = `mailto:${to}?subject=${subject}&body=Name: ${name}%0AEmail: ${email}%0AMessage:%0A${message}`;
-
-        window.location.href = mailtoLink;
-    });
+    emailjs.sendForm('service_8lest3i', 'template_z90rqpt', this)
+        .then(function(response) {
+            alert('Success! Your message has been sent.');
+            console.log('Sent successfully', response);
+        }, function(error) {
+            alert('Failed to send message. Please try again.');
+            console.log('Failed', error);
+        });
+});
